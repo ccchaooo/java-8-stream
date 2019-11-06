c5068cca2c45b34f91cfb057bcab8a9e111878e0
dengchao <645111208@qq.com>
2019-11-06 10:05:32 +0800

    精简

--- src/test/java/CollectorsDemoTest.java
+++ src/test/java/CollectorsDemoTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.junit.Assert.*;
 
@@ -22,43 +21,6 @@ public class CollectorsDemoTest {
     }
 
     @Test
-    public void toList() {
-        List<Integer> lengthList = strings
-                .stream()
-                .map(String::length)
-                .collect(Collectors.toList());
-        assertEquals(1, (int) lengthList.get(0));
-        assertEquals(2, (int) lengthList.get(1));
-        assertEquals(3, (int) lengthList.get(2));
-        assertEquals(4, (int) lengthList.get(3));
-    }
-
-    @Test
-    public void toSet() {
-        Set<Integer> stringSet = this.strings
-                .stream()
-                .map(String::length)
-                .collect(Collectors.toSet());
-        assertEquals(3, stringSet.size());
-    }
-
-    @Test
-    public void joining() {
-        String joinedString = this.strings
-                .stream().collect(Collectors.joining());
-        assertEquals("abbcccddd", joinedString);
-    }
-
-    @Test
-    public void joining_with_delimiter() {
-        CharSequence delimiter = "---";
-        String joinedString = this.strings
-                .stream().collect(Collectors.joining(delimiter));
-        assertEquals("a---bb---ccc---ddd", joinedString);
-    }
-
-
-    @Test
     public void joining_with_delimiter_prefix_suffix() {
         CharSequence delimiter = "---";
         CharSequence prefix = "begin->";
@@ -67,12 +29,5 @@ public class CollectorsDemoTest {
                 .stream().collect(Collectors.joining(delimiter, prefix, suffix));
         assertEquals("begin->a---bb---ccc---ddd->end", joinedString);
     }
-
-    @Test
-    public void mapping() {
-        this.strings
-                .stream().collect(Collectors.mapping(String::length),Integer.class);
-    }
-
 
 }
